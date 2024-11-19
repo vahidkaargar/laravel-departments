57a284019682e137d1420e1772476fc01f70e636
Vahid Kaargar <vahidkaargar@gmail.com>
2024-11-20 02:47:09 +0330

    Update DepartmentsServiceProvider.php

--- src/ServiceProviders/DepartmentsServiceProvider.php
+++ src/ServiceProviders/DepartmentsServiceProvider.php
@@ -1,24 +1,42 @@
 <?php
+
 namespace vahidkaargar\LaravelDepartments\ServiceProviders;
 
 use Illuminate\Support\ServiceProvider;
+use vahidkaargar\LaravelDepartments\Departments;
 
 class DepartmentsServiceProvider extends ServiceProvider
 {
-    public function register()
+    public function register(): void
     {
+        $this->singletonFromDepartmentsClass();
+
+        // load files
         $this->mergeConfigFrom(__DIR__ . '/../Configs/departments.php', 'departments');
-        $this->loadMigrationFrom(__DIR__ . '/../Database/Migrations');
+        $this->loadMigrationsFrom(__DIR__ . '/../Database/Migrations');
     }
 
-    public function boot()
+    public function boot(): void
     {
         if ($this->app->runningInConsole()) {
+
             $this->publishes([
                 __DIR__ . '/../Configs/departments.php' => config_path('departments.php'),
             ], 'departments-config');
+
+            $this->publishes([
+                __DIR__ . '/../Database/Migrations' => database_path('migrations'),
+            ], 'departments-migrations');
+
         }
 
         require_once __DIR__ . '/../Helpers/helpers.php';
+    }
+
+    private function singletonFromDepartmentsClass(): void
+    {
+        $this->app->singleton('departments', function ($app) {
+            return new Departments();
+        });
     }
 }
