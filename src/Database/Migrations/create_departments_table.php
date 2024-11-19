8071cfe12e55354ecefcc1c9d9a9c2cbd135cd0d
Vahid Kaargar <vahidkaargar@gmail.com>
2024-11-20 02:44:07 +0330

    Refactor departments migration

--- src/Database/Migrations/create_departments_table.php
+++ src/Database/Migrations/create_departments_table.php
@@ -1,17 +1,20 @@
 <?php
 
+namespace vahidkaargar\LaravelDepartments\Database\Migrations;
+
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateDepartmentsTable extends Migration {
+return new class extends Migration {
     public function up(): void
     {
         Schema::create('departments', function (Blueprint $table) {
             $table->id();
             $table->string('title');
-            $table->text('body')->nullable();
-            $table->boolean('is_active')->default(true);
+            $table->longText('body')->nullable();
+            $table->boolean('is_active')->index()->default(false);
+            $table->softDeletes();
             $table->timestamps();
         });
     }
