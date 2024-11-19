122a5cd9fac8b55659beaa5c8fefea4b9a53bed9
Hamidreza <momenih333@gmail.com>
2024-11-20 00:41:04 +0330

    create model for departments table

---
+++ src/Models/Department.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace vahidkaargar\LaravelDepartments\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Department extends Model
+{
+    protected $fillable = ['title', 'body', 'is_active'];
+}
